722e791cde21100b1a6e559c5cdcf6b0dad3aa33
Lisber Victores <lvpompa@gmail.com>
2021-06-06 17:01:14 -0300

    create cobrinha

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
+let box = 32;// quantidade de box do tabuleiro
+let snake = [];
 
-function criarBG() {
-    context.fillStyle = "Lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box,
 }
 
+// crndo tabuleiro da cobrinha
+function criarBG() {
+    context.fillStyle = "Lightgreen";// cor 
+    context.fillRect(0, 0, 16 * box, 16 * box);// tamanho
+}
+
+//função para criar a cobrinha 
+function criarCobrinha() {
+    for (i=0; i < snake.length; i++) {
+        context.fillStyle = "green";// cor para a cobrinha
+        context.fillRect(snake[i].x, snake[i].y, box, box);//definendo posição da cobrinha
+        
+    }
+}
 criarBG();
+criarCobrinha();
